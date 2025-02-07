898acf7fb852d2af0846d768952f97c8138cd55b
fikrynuhuman183 <fikryfikrynuhuman@gmail.com>
2025-02-07 12:42:20 +0530

    qr code

--- web3/script.js
+++ web3/script.js
@@ -185,7 +185,7 @@ function checkGroupName() {
         let qrPath = `/img/QR_codes/${regNum}.png`;
         qrContainer.innerHTML = `
             
-            <a href="${qrPath}" download>
+            <a href="${qrPath}" >
                 <button style="margin-top: 10px; background-color: green; color: white; border: none; padding: 10px 15px; border-radius: 5px; cursor: pointer;">Download QR Code</button>
             </a>
         `;
